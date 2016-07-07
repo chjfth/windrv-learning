442113cad3a02e60d5714df4360c02c930947bc8
chj <chj@34977f82-1c41-1c46-8be5-c6b895e38539>
2016-07-07 07:13:48 +0000

    git-svn-id: svn://10.22.3.84/windrv-learning/trunk@60 34977f82-1c41-1c46-8be5-c6b895e38539

--- wdk7-samples/toaster/wdm/func/featured1/toaster.c
+++ wdk7-samples/toaster/wdm/func/featured1/toaster.c
@@ -1,7 +1,5 @@
 /*++
-
 Copyright (c) Microsoft Corporation.  All rights reserved.
-
     THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
     KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
     IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
@@ -50,7 +48,6 @@ Environment:
     Kernel mode
 
 Revision History:
-
 --*/
 #include "toaster.h"
 
