81428803f1af52119776a721c3d8c996395a6144
chj <chj@34977f82-1c41-1c46-8be5-c6b895e38539>
2016-07-12 04:03:04 +0000

    git-svn-id: svn://10.22.3.84/windrv-learning/trunk@82 34977f82-1c41-1c46-8be5-c6b895e38539

--- wdk7-samples/cancel/exe-once/testapp.cpp
+++ wdk7-samples/cancel/exe-once/testapp.cpp
@@ -2,26 +2,26 @@
 
 Copyright (c) Microsoft Corporation.  All rights reserved.
 
-    THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
-    KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-    IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
-    PURPOSE.
+	THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+	KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
+	PURPOSE.
 
 Module Name:
 
-    testapp.c
+	testapp.c
 
 Abstract:
 
 Environment:
 
-    User mode Win32 console application
+	User mode Win32 console application
 --*/
 
 //
 // Annotation to indicate to prefast that this is nondriver user-mode code.
 //
-                                                  
+												  
 #include <DriverSpecs.h>
 __user_code  
 
@@ -47,21 +47,21 @@ HANDLE hThreads[MAXTHREADS];
 //
 
 VOID CALLBACK CompletionRoutine(
-    DWORD errorcode,
-    DWORD bytesTransfered,
-    LPOVERLAPPED ov
-    );
+	DWORD errorcode,
+	DWORD bytesTransfered,
+	LPOVERLAPPED ov
+	);
 
 DWORD
 WINAPI Reader(
-    PVOID
-    );
+	PVOID
+	);
 
 BOOLEAN
 SetupDriverName(
-    __inout_bcount_full(BufferLength) PCHAR DriverLocation,
-    __in ULONG BufferLength
-    );
+	__inout_bcount_full(BufferLength) PCHAR DriverLocation,
+	__in ULONG BufferLength
+	);
 
 //
 // Main function
@@ -69,114 +69,118 @@ SetupDriverName(
 
 VOID __cdecl
 main(
-    __in ULONG argc,
-    __in_ecount(argc) PCHAR argv[]
-    )
+	__in ULONG argc,
+	__in_ecount(argc) PCHAR argv[]
+	)
 {
-    ULONG i, Id;
-    ULONG   NumberOfThreads = 1;
-    DWORD errNum = 0;
-    TCHAR driverLocation[MAX_PATH] = {'\0'};
+	ULONG i, Id;
+	ULONG   NumberOfThreads = 1;
+	DWORD errNum = 0;
+	TCHAR driverLocation[MAX_PATH] = {'\0'};
 
-    if (argc >= 2  && (argv[1][0] == '-' || isalpha((unsigned char)argv[1][0])))
-    {
-        puts("Usage:testapp <NumberOfThreads>\n");
-        return;
-    }
-    else if (argc >= 2 && ((NumberOfThreads = atoi(argv[1])) > MAXTHREADS))
-    {
-        printf("Invalid option:Only a maximun of %d threads allowed.\n", MAXTHREADS);
-        return;
-    }
-
-    //
-    // Try to connect to driver.  If this fails, try to load the driver
-    // dynamically.
-    //
-    if ((hDevice = CreateFile("\\\\.\\CancelSamp",
-                                 GENERIC_READ,
-                                 0,
-                                 NULL,
-                                 OPEN_EXISTING,
-                                 FILE_FLAG_OVERLAPPED,
-                                 NULL
-                                 )) == INVALID_HANDLE_VALUE) 
+	if (argc >= 2  && (argv[1][0] == '-' || isalpha((unsigned char)argv[1][0])))
 	{
-        errNum = GetLastError();
+		puts("Usage:testapp <NumberOfThreads>\n");
+		return;
+	}
+	else if (argc >= 2 && ((NumberOfThreads = atoi(argv[1])) > MAXTHREADS))
+	{
+		printf("Invalid option:Only a maximun of %d threads allowed.\n", MAXTHREADS);
+		return;
+	}
 
-        if (errNum != ERROR_FILE_NOT_FOUND) {
-            printf("CreateFile failed!  Error = %d\n", errNum);
-            return ;
-        }
+	//
+	// Try to connect to driver.  If this fails, try to load the driver
+	// dynamically.
+	//
+	if ((hDevice = CreateFile("\\\\.\\CancelSamp",
+								 GENERIC_READ,
+								 0,
+								 NULL,
+								 OPEN_EXISTING,
+								 FILE_FLAG_OVERLAPPED,
+								 NULL
+								 )) == INVALID_HANDLE_VALUE) 
+	{
+		errNum = GetLastError();
 
-        // Setup full path to driver name.
-        if (!SetupDriverName(driverLocation, sizeof(driverLocation))) {
-            return ;
-        }
+		if (errNum != ERROR_FILE_NOT_FOUND) {
+			printf("CreateFile failed!  Error = %d\n", errNum);
+			return ;
+		}
 
-        //
-        // Install driver.
-        //
-        if (!ManageDriver(DRIVER_NAME,
-                          driverLocation,
-                          DRIVER_FUNC_INSTALL
-                          )) 
-		{
-            printf("Unable to install driver. \n");
-
-            // Error - remove driver.
-            ManageDriver(DRIVER_NAME,
-                         driverLocation,
-                         DRIVER_FUNC_REMOVE
-                         );
-            return;
-        }
+		// Setup full path to driver name.
+		if (!SetupDriverName(driverLocation, sizeof(driverLocation))) {
+			return ;
+		}
 
 		//
-        // Try to open the newly installed driver.
-        //
-        hDevice = CreateFile( "\\\\.\\CancelSamp",
-                GENERIC_READ,
-                0,
-                NULL,
-                OPEN_EXISTING,
-                FILE_FLAG_OVERLAPPED,
-                NULL);
-
-        if ( hDevice == INVALID_HANDLE_VALUE )
+		// Install driver.
+		//
+		if (!ManageDriver(DRIVER_NAME,
+						  driverLocation,
+						  DRIVER_FUNC_INSTALL
+						  )) 
 		{
-            printf ( "Error: CreatFile Failed(2nd-try) : %d\n", GetLastError());
-            return;
-        }
-    }
+			printf("Unable to install driver. \n");
 
-    printf("Number of threads : %d\n", NumberOfThreads);
+			// Error - remove driver.
+			ManageDriver(DRIVER_NAME,
+						 driverLocation,
+						 DRIVER_FUNC_REMOVE
+						 );
+			return;
+		}
 
-    printf("Enter 'q' to exit gracefully:");
+		//
+		// Try to open the newly installed driver.
+		//
+		hDevice = CreateFile( "\\\\.\\CancelSamp",
+				GENERIC_READ,
+				0,
+				NULL,
+				OPEN_EXISTING,
+				FILE_FLAG_OVERLAPPED,
+				NULL);
 
-    for(i=0; i < NumberOfThreads; i++)
-    {
-        hThreads[i] = CreateThread( NULL,      // security attributes
-                                    0,         // initial stack size
-                                    Reader,    // thread function
-                                    NULL,      // arg to Reader thread
-                                    0,         // creation flags
-                                    (LPDWORD)&Id); // returned thread id
+		if ( hDevice == INVALID_HANDLE_VALUE )
+		{
+			printf ( "Error: CreatFile Failed(2nd-try) : %d\n", GetLastError());
+			return;
+		}
+	}
 
-        if ( NULL == hThreads[i] ) {
-            printf( " Error CreateThread[%d] Failed: %d\n", i, GetLastError());
-            ExitProcess ( 1 );
-        }
-    }
+	printf("========================================================\n");
+	printf("Chj Note: Each thread will do ReadFileEx() only once.\n");
+	printf("========================================================\n");
+
+	printf("Number of threads : %d\n", NumberOfThreads);
+
+	printf("Enter 'q' to exit gracefully:");
+
+	for(i=0; i < NumberOfThreads; i++)
+	{
+		hThreads[i] = CreateThread( NULL,      // security attributes
+									0,         // initial stack size
+									Reader,    // thread function
+									NULL,      // arg to Reader thread
+									0,         // creation flags
+									(LPDWORD)&Id); // returned thread id
+
+		if ( NULL == hThreads[i] ) {
+			printf( " Error CreateThread[%d] Failed: %d\n", i, GetLastError());
+			ExitProcess ( 1 );
+		}
+	}
 
 	int quitchar = getchar();
 
-    if(quitchar == 'q') // do graceful thread-exit
-    {
-        ExitFlag = TRUE;
-        WaitForMultipleObjects( NumberOfThreads, hThreads, TRUE, INFINITE);
+	if(quitchar == 'q') // do graceful thread-exit
+	{
+		ExitFlag = TRUE;
+		WaitForMultipleObjects( NumberOfThreads, hThreads, TRUE, INFINITE);
 		CloseHandle(hDevice);
-    }
+	}
 	else // do force-exit
 	{
 		CloseHandle(hDevice); // close device handle while it is still in use
@@ -186,52 +190,60 @@ main(
 	for(i=0; i < NumberOfThreads; i++)
 		CloseHandle(hThreads[i]);
 
-    //
-    // Unload the driver.  Ignore any errors.
-    //
-    BOOLEAN b = ManageDriver(DRIVER_NAME,
-                 driverLocation,
-                 DRIVER_FUNC_REMOVE
-                 );
+	//
+	// Unload the driver.  Ignore any errors.
+	//
+	BOOLEAN b = ManageDriver(DRIVER_NAME,
+				 driverLocation,
+				 DRIVER_FUNC_REMOVE
+				 );
 
-    ExitProcess(b ? 0 : 1);
+	ExitProcess(b ? 0 : 1);
 }
 
 
 DWORD WINAPI Reader(PVOID dummy )
 {
-    ULONG data;
-    OVERLAPPED ov;
+	ULONG data = 0x11223344;
+	OVERLAPPED ov;
 	DWORD tid = GetCurrentThreadId();
 
-    UNREFERENCED_PARAMETER(dummy);
+	UNREFERENCED_PARAMETER(dummy);
 
-    while(!ExitFlag)
-    {
-        ZeroMemory( &ov, sizeof(ov) );
-        ov.Offset = 0;
-        ov.OffsetHigh = 0;
+	ZeroMemory( &ov, sizeof(ov) );
+	ov.Offset = 0;
+	ov.OffsetHigh = 0;
 
-        if (!ReadFileEx(hDevice, (PVOID)&data, sizeof(ULONG),  &ov, CompletionRoutine))
-        {
-            printf ( "(tid %d)ReadFileEx failed, winerr=%d\n", tid, GetLastError());
-            break; //ExitProcess(1);
-        }
-        SleepEx(INFINITE, TRUE);
-    }
+	if (!ReadFileEx(hDevice, (PVOID)&data, sizeof(ULONG),  &ov, CompletionRoutine))
+	{
+		printf ( "(tid=%d)ReadFileEx failed, winerr=%d\n", tid, GetLastError());
+		ExitThread(0);
+	}
+	
+	bool io_complete = false;
+	while(!ExitFlag)
+	{
+		DWORD re = SleepEx(100, TRUE);
+		if(re==WAIT_IO_COMPLETION)
+		{
+			printf("(tid=%d)SleepEx() returned with WAIT_IO_COMPLETION. Work done.\n", tid);
+			io_complete = true;
+			break;
+		}
+	}
 
-    printf("(tid %d)Exiting thread.\n", tid);
-    ExitThread(0);
+	printf("(tid=%d)Calling ExitThread(0). %s\n", tid, io_complete?"":"(without IO completion)");
+	ExitThread(0);
 }
 
 
 VOID CALLBACK CompletionRoutine(
-    DWORD errorcode,
-    DWORD bytesTransfered,
-    LPOVERLAPPED ov
-    )
+	DWORD errorcode,
+	DWORD bytesTransfered,
+	LPOVERLAPPED ov
+	)
 {
-    UNREFERENCED_PARAMETER(ov);
+	UNREFERENCED_PARAMETER(ov);
 
 	if(!errorcode)
 	{
@@ -243,54 +255,54 @@ VOID CALLBACK CompletionRoutine(
 		fprintf(stdout, "Thread %d CompletionRoutine got error: winerr=%d\n",
 			GetCurrentThreadId(), errorcode);
 	}
-    return;
+	return;
 }
 
 
 BOOLEAN
 SetupDriverName(
-    __inout_bcount_full(BufferLength) PCHAR DriverLocation,
-    __in ULONG BufferLength
-    )
+	__inout_bcount_full(BufferLength) PCHAR DriverLocation,
+	__in ULONG BufferLength
+	)
 {
-    HANDLE fileHandle;
-    DWORD driverLocLen = 0;
+	HANDLE fileHandle;
+	DWORD driverLocLen = 0;
 
-    // Get the current directory.
-    driverLocLen = GetCurrentDirectory(BufferLength,
-                                       DriverLocation
-                                       );
-    if (driverLocLen == 0) {
-        printf("GetCurrentDirectory failed!  Error = %d \n", GetLastError());
-        return FALSE;
-    }
+	// Get the current directory.
+	driverLocLen = GetCurrentDirectory(BufferLength,
+									   DriverLocation
+									   );
+	if (driverLocLen == 0) {
+		printf("GetCurrentDirectory failed!  Error = %d \n", GetLastError());
+		return FALSE;
+	}
 
-    // Setup path name to driver file.
-    if (FAILED( StringCbCat(DriverLocation, BufferLength, "\\"DRIVER_NAME".sys") )) {
-        return FALSE;
-    }
+	// Setup path name to driver file.
+	if (FAILED( StringCbCat(DriverLocation, BufferLength, "\\"DRIVER_NAME".sys") )) {
+		return FALSE;
+	}
 
-    //
-    // Insure driver file is in the specified directory.
-    //
-    if ((fileHandle = CreateFile(DriverLocation,
-                                 GENERIC_READ,
-                                 0,
-                                 NULL,
-                                 OPEN_EXISTING,
-                                 FILE_ATTRIBUTE_NORMAL,
-                                 NULL
-                                 )) == INVALID_HANDLE_VALUE) 
+	//
+	// Insure driver file is in the specified directory.
+	//
+	if ((fileHandle = CreateFile(DriverLocation,
+								 GENERIC_READ,
+								 0,
+								 NULL,
+								 OPEN_EXISTING,
+								 FILE_ATTRIBUTE_NORMAL,
+								 NULL
+								 )) == INVALID_HANDLE_VALUE) 
 	{
-        printf("%s.sys is not loaded.\n", DRIVER_NAME);
-        return FALSE;
-    }
+		printf("%s.sys is not loaded.\n", DRIVER_NAME);
+		return FALSE;
+	}
 
-    // Close open file handle.
-    if (fileHandle) {
-        CloseHandle(fileHandle);
-    }
+	// Close open file handle.
+	if (fileHandle) {
+		CloseHandle(fileHandle);
+	}
 
-    return TRUE;
+	return TRUE;
 }   // SetupDriverName
 
